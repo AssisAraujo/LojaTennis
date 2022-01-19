0b3112063db6edc538f773b232ffba26cb0cb0bf
Assis Souza Araujo <54477660+AssisAraujo@users.noreply.github.com>
2022-01-18 21:03:48 -0400

    Router

---
+++ src/pages/Detail/index.js
@@ -0,0 +1,11 @@
+import rect from 'react';
+import {View, Text} from 'react-native';
+
+
+export default function Detail(){
+    return(
+        <View>
+            <Text style={{marginTop: 60, fontSize: 25,}}>Pagina Detalhe</Text>
+        </View>
+    );
+}
